f20b45d7ebc0c6897dc1f2f9a466902e1aa25546
aidenkwong <aiden.kwong@aigniter.com>
2022-06-06 17:43:59 +0800

    remove ad components

--- src/api/youtube-api.js
+++ src/api/youtube-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:5000"
+      ? "http://localhost:5001"
       : "https://youtube-popular-video-api.herokuapp.com",
 });
 
